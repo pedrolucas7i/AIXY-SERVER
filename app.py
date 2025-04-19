24004f303a6e902d15ff9bf98a4d1019a50627fd
pedrolucas7i <pedrolucas.core7i@gmail.com>
2025-04-19 12:49:22 +0100

    Added .env

--- app.py
+++ app.py
@@ -130,4 +130,4 @@ def text_to_speech(message):
 
 if __name__ == "__main__":
     # Run the Flask app on all available IP addresses (0.0.0.0) and port 5000
-    app.run(debug=True, host="0.0.0.0", port=5000, use_reloader=False)
+    app.run(debug=True, host="0.0.0.0", port=os.environ('PORT'), use_reloader=False)
